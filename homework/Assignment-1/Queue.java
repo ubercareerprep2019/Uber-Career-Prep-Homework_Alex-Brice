eb6c00b2db737d6c73ce64f66142980421e5226a
Alex Brice Horimbere <44306134+brice1997@users.noreply.github.com>
2019-05-26 16:13:19 -0400

    Source file for Queue class of Part3

---
+++ homework/Assignment-1/Queue.java
@@ -0,0 +1,36 @@
+public class Queue{
+    
+    //instance variable
+    
+    MyStack queue; 
+    
+    public Queue(){
+        queue = new MyStack(); 
+    }
+    
+    public void enqueue(Integer elt){
+        queue.push(elt); 
+    }
+    
+    public Integer dequeue(){
+        //create a temporary MyStack
+        
+        MyStack tmp = new MyStack ();
+        Integer deq; 
+        
+        while(!queue.isEmpty()){
+            Integer r = queue.pop(); 
+            tmp.push(r); 
+        }
+        
+        deq = tmp.pop(); //dequeue-ing the bottom element, which was the "first"
+        
+        while (!tmp.isEmpty()){
+            Integer s = tmp.pop(); 
+            queue.push(s); 
+        }
+        
+        return deq;   
+        
+    }
+}
